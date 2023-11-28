307850531281f109772cc91e4f377d5eda8a307f
TatakRahab <levi.sunio@gmail.com>
2023-11-28 14:14:20 +0800

    files upload

--- index.js
+++ index.js
@@ -24,16 +24,29 @@ function replacePasswordFormWithBox() {
     var box = document.createElement('div');
     box.className = 'access-granted';
     box.style.border = '2px solid #00FF19';
-    box.style.padding = '10px';
     box.style.margin = '20px';
+    box.style.padding = '30px 30px';
     box.style.color = '#00FF19';
     box.style.textAlign = 'center';
     box.style.fontFamily = 'Courier New, Courier, serif';
-    box.style.position = 'fixed';
-    box.style.top = '50%';
+    box.style.position = 'absolute';
+    box.style.top = '45%';
     box.style.left = '50%';
     box.style.transform = 'translate(-50%, -50%)';
     box.innerHTML = 'Access Granted';
+    box.style.zIndex = '30';
+    box.style.background = 'none';
+    box.style.fontSize = '25px';
 
     document.body.appendChild(box);
+
+    // Delay the appearance of the "Access Granted" box
+    setTimeout(function () {
+        box.style.opacity = 1;
+    }, 100); // Adjust the delay time as needed
+
+     setTimeout(function () {
+                fadeOutElements([videoContainer]);
+                document.getElementById("access-granted").play();
+            }, 200); // Adjust the delay as needed
 }
